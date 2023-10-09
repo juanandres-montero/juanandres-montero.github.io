5d096476d3c04c0b859dfda655b58887f177b7c3
juanandres-montero <juanandres.montero@outlook.com>
2023-10-09 01:51:17 -0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle')
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+        
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
